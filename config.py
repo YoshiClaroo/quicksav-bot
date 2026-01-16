dfbfb0504b6b7209923075d244c2415c263a3db7
YoshiClaroo <braianm313@gmail.com>
2026-01-16 17:40:37 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SMARTLINK = os.getenv("SMARTLINK")
+token = os.getenv("BOT_TOKEN")
+smartlink = os.getenv("SMARTLINK")
 logs = 2390049  # logs channel id, if none set to None
 max_filesize = 50000000  # bytes
 output_folder="/tmp/yt-dlp-telegram"
